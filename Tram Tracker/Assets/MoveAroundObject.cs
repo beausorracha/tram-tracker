bfa06f6b4fdf5fa467dc33d3d05e271d12af0db6
ChetsudaP <158053015+ChetsudaP@users.noreply.github.com>
2025-03-17 16:56:16 +0700

    MoveCameraAroundTram

---
+++ Tram Tracker/Assets/MoveAroundObject.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAroundObject : MonoBehaviour
+{
+    [SerializeField]
+    private float _mouseSensitivity = 3.0f;
+
+    private float _rotationY;
+    private float _rotationX;
+
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private float _distanceFromTarget = 3.0f;
+
+    private Vector3 _currentRotation;
+    private Vector3 _smoothVelocity = Vector3.zero;
+
+    [SerializeField]
+    private float _smoothTime = 0.2f;
+
+    [SerializeField]
+    private Vector2 _rotationXMinMax = new Vector2(-40, 40);
+
+    void Update()
+    {
+       if (Input.GetMouseButton(0)) {
+        Move();
+       }
+
+    }
+
+    void Move()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity;
+        // float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity;
+
+        _rotationY += mouseX;
+        // _rotationX += mouseY;
+
+        // Apply clamping for x rotation 
+        _rotationX = Mathf.Clamp(_rotationX, _rotationXMinMax.x, _rotationXMinMax.y);
+
+        Vector3 nextRotation = new Vector3(_rotationX, _rotationY);
+
+        // Apply damping between rotation changes
+        _currentRotation = Vector3.SmoothDamp(_currentRotation, nextRotation, ref _smoothVelocity, _smoothTime);
+        transform.localEulerAngles = _currentRotation;
+
+        // Substract forward vector of the GameObject to point its forward vector to the target
+        Vector3 fixpos = new Vector3(transform.position.x, transform.position.y+100, transform.position.z);
+        fixpos = _target.position - transform.forward * _distanceFromTarget; 
+        fixpos.y = Mathf.Max(fixpos.y, 3.4f);
+        transform.position = fixpos;
+    }
+}
